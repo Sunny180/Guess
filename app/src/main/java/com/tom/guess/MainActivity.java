09f291d8bbfb8fbd599735384ae7a8cbf3ebee16
miia <miia@com.tw>
2019-10-29 16:23:13 +0800

    456

--- app/src/main/java/com/tom/guess/MainActivity.java
+++ app/src/main/java/com/tom/guess/MainActivity.java
@@ -1,16 +1,19 @@
 package com.tom.guess;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,7 +22,9 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     int secret = new Random().nextInt(10)+1;
+    int counter;
     private EditText edNumber;
+    private TextView edCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         edNumber = findViewById(R.id.ed_number);
+        edCounter = findViewById(R.id.counter);
+        edCounter.setText(String.valueOf(counter));
         FloatingActionButton fab = findViewById(R.id.fab);
+        Button button = findViewById(R.id.button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,11 +48,35 @@ public class MainActivity extends AppCompatActivity {
     public void guess(View view){
         int n =Integer.parseInt(edNumber.getText().toString());
         edNumber.setText(Integer.toString(n));
+        counter++;
+        edCounter.setText(counter+"");
             if (n < secret){
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Ha")
+                        .setMessage("Bigger")
+                        .setPositiveButton("OK",null)
+                        .show();
                 Toast.makeText(MainActivity.this, "Bigger", Toast.LENGTH_SHORT).show();
             }else if (n>secret){
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Ha")
+                        .setMessage("Smaller")
+                        .setPositiveButton("OK",null)
+                        .show();
                 Toast.makeText(MainActivity.this, "Smaller", Toast.LENGTH_SHORT).show();
             }else {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Jo li hi")
+                        .setMessage("Bingo")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                secret = new Random().nextInt(10)+1;
+                                counter = 0;
+                                edCounter.setText(counter + "");
+                            }
+                        })
+                        .show();
                 Toast.makeText(MainActivity.this, "Bingo", Toast.LENGTH_SHORT).show();
             }
 
